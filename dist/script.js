224662fdfe43ead2f1bca52d35278d2a049c79a1
Rohimat Mustapha <rohimat.m1800276@st.futminna.edu.ng>
2024-01-11 23:40:23 +0100

    added some changes

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navlink = document.querySelector(".navLinks");
+const dropdownLink = document.querySelector(".js-dropdown-link");
+const dropdownLink2 = document.querySelector(".js-dropdown-link-2");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
@@ -8,10 +10,19 @@ hamburger.addEventListener("click", () => {
 document.querySelectorAll(".link").forEach((link) =>
   link.addEventListener("click", () => {
     hamburger.classList.remove("active");
-    navlink.classList.remove("active");
   })
 );
 document.querySelector(".main").addEventListener("click", () => {
   hamburger.classList.remove("active");
   navlink.classList.remove("active");
 });
+document.querySelectorAll(".js-dropdown").forEach((dropdown) =>
+  dropdown.addEventListener("click", () => {
+    dropdownLink.classList.toggle("activated");
+  })
+);
+document.querySelectorAll(".js-dropdown2").forEach((dropdown) =>
+  dropdown.addEventListener("click", () => {
+    dropdownLink2.classList.toggle("activated");
+  })
+);
